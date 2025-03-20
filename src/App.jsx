0d8c3cf363529239f7cdda9ce21eaa05fbbc60d2
Javier Emmanuel Granados Pardo <javieremmanuelgranados@outlook.com>
2025-03-20 09:37:27 -0700

    Se modificó el archivo App.jsx: Se agregó un useEffect a Count

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    if (count > 10) {
+      setCount(10);
+      alert("Alcanzaste el límite de 10")
+    }
+  }, [count]);
 
   return (
     <>
@@ -21,15 +28,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
